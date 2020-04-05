9b98ab3c5ed95f21e8048e08bc0f5cefcb3ac48e
Bobby Odian <odianrobert@yahoo.com>
2020-04-04 21:27:04 -0400

    Updates

---
+++ precache-manifest.de084eae5bf8be02691ac4bb10bdb8a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5874c7687b96367fa63419755aebfb",
+    "url": "/Odianrobert-Employee-Directory/index.html"
+  },
+  {
+    "revision": "6925307aad518d20bb38",
+    "url": "/Odianrobert-Employee-Directory/static/css/main.16f2466d.chunk.css"
+  },
+  {
+    "revision": "06611d205afe6343c854",
+    "url": "/Odianrobert-Employee-Directory/static/js/2.717472a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Odianrobert-Employee-Directory/static/js/2.717472a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6925307aad518d20bb38",
+    "url": "/Odianrobert-Employee-Directory/static/js/main.81c7c0b3.chunk.js"
+  },
+  {
+    "revision": "86161c50feb0fc1a2889",
+    "url": "/Odianrobert-Employee-Directory/static/js/runtime-main.559d01d4.js"
+  }
+]);
