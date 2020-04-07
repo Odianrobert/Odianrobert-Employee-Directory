a74be4d88515081ffafe882aee3bcf0f3a758f02
Bobby Odian <odianrobert@yahoo.com>
2020-04-06 22:41:41 -0400

    trying to solve 404 error

---
+++ build/precache-manifest.e9980a08adfa7786024c0eb483ce6ef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac05565cc178a2d660b3d7dc3682c7b7",
+    "url": "/Odianrobert-Employee-Directory/index.html"
+  },
+  {
+    "revision": "49590782acbd9e4f92a3",
+    "url": "/Odianrobert-Employee-Directory/static/css/main.16f2466d.chunk.css"
+  },
+  {
+    "revision": "06611d205afe6343c854",
+    "url": "/Odianrobert-Employee-Directory/static/js/2.717472a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Odianrobert-Employee-Directory/static/js/2.717472a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49590782acbd9e4f92a3",
+    "url": "/Odianrobert-Employee-Directory/static/js/main.a205bb03.chunk.js"
+  },
+  {
+    "revision": "86161c50feb0fc1a2889",
+    "url": "/Odianrobert-Employee-Directory/static/js/runtime-main.559d01d4.js"
+  }
+]);
